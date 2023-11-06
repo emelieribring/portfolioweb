a86656ce9c83a11ba75f44f173a941a865b6cd41
emelieribring <emelieribring@hotmail.com>
2023-11-07 00:53:20 +0100

    page two

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -38,7 +38,56 @@ function Landing() {
     <div className='overlay'></div>
   </div> 
   <div className='secondWrapper'>
-
+    <p className='headingHelpYou'>How can I help you?</p>
+    <div className='hidden' id='infoBox'>
+        <div className='infoLeft'>
+            <p>Things I do</p>
+            <ul>
+                <li>Front-end</li>
+                <li>Back-end</li>
+                <li>Web3 / Blockchain</li>
+                <li>Smart Contracts</li>
+                <li>API calls</li>
+                <li>Web Design</li>
+                <li>Photoshop</li>
+                <li>Wordpress</li>
+                <li>Agile Methodology</li>
+                <li>Photograph</li>
+            </ul>
+            <p>Programming languages I know </p>
+            <ul>
+                <li>HTML5</li>
+                <li>CSS/SCSS</li>
+                <li>Javascript</li>
+            </ul>
+        </div>
+        <div className='infoRight'>
+            <p>Frameworks</p>
+            <ul>
+                <li>React</li>
+                <li>Solidity</li>
+                <li>Phyton</li>
+                <li>Next.js</li>
+            </ul>
+            <p>Programs I work in</p>
+            <ul>
+                <li>Visual Studio Code</li>
+                <li>Github/Git</li>
+                <li>Truffle</li>
+                <li>Photoshop</li>
+                <li>Canva</li>
+                <li>Figma</li>
+                <li>InDesign</li>
+            </ul>
+        </div>
+    </div>
+    <p className='headingHelpYou'>Anymore questions?</p>
+    <div className='hidden' id='infoBox'>
+        <div className='contact'>
+            <p>Contact me</p>
+            <p>email: <br /> emelieribring@hotmail.com <br /> <br />LinkedIn: emelieribring <br /> Github: emelieribring <br /><br  />Tel: 073 735 35 00</p>
+        </div>
+    </div>
   </div>
   </>
   );
