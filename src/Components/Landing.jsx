f15a60ddfac31795fb2a57cfe441e35672648f45
Emelie Ribring <100706474+emelieribring@users.noreply.github.com>
2023-11-10 13:45:18 +0100

    Update Landing.jsx

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -66,6 +66,8 @@ function Landing() {
             <ul>
                 <li>React</li>
                 <li>Solidity</li>
+                <li>Phyton</li>
+                <li>Next.js</li>
             </ul>
             <p>Programs I work in</p>
             <ul>
